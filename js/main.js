887c808831c7afd2eb8b5516fc5c93f9fc86e022
RookieYang <yy335281965@gmail.com>
2018-11-20 17:31:56 +0800

    添加代码

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+$('.images > img:nth-child(4)').addClass('enter')
