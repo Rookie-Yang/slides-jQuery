2246180a3a2b705d62fe6a10c48a210539ff0c79
RookieYang <yy335281965@gmail.com>
2018-11-21 16:26:04 +0800

    更新代码

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ $('.images > img:nth-child(1)').addClass('current')
 $('.images > img:nth-child(2)').addClass('enter')
 $('.images > img:nth-child(3)').addClass('enter')
 $('.images > img:nth-child(4)').addClass('enter')
+
+let index = 1
+setInterval(()=>{
+  $(`.images > img:nth-child(${x(index)})`).removeClass('current').addClass('leave')
+  .one('transitionend',(e)=>{
+    $(e.currentTarget).removeClass('leave').addClass('enter')
+  })  
+  $(`.images > img:nth-child(${x(index+1)})`).removeClass('enter').addClass('current')
+  index++
+},3000)
+
+function x(n){
+  if(n>4){
+   n%=4
+   if(n===0){
+     n=4
+   }
+  }
+  return n
+}
