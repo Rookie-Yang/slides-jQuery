eeec1f6b4b23e6e7b06c3ac3bcef762c0684de6a
RookieYang <yy335281965@gmail.com>
2018-11-30 17:40:54 +0800

    重写

--- js/main.js
+++ js/main.js
@@ -1,62 +1,31 @@
+$buttons = $('#buttonWrapper > button')
+$slides = $('#slides')
+$images = $slides.children('img')
+$firstCopy = $images.eq(0).clone(true)
+$lastCopy = $images.eq($images.length-1).clone(true)
 
+$slides.append($firstCopy)
+$slides.prepend($lastCopy)
+let current = 0
+$buttons.eq(0).on('click',function(e){
+  if(current ===3){
 
-let index
-init()
-
-let timer=setInterval(()=>{
-  makeLeave(getImage(index))
-  .one('transitionend',(e)=>{
-    makeEnter($(e.currentTarget))
-  })  
-  makeCurrent(getImage(index+1))
-  index++
-},1000)
-
-document.addEventListener('visibilitychange',function(e){
-  if(document.hidden){
-    window.clearInterval(timer)
-  }else{
-    timer=setInterval(()=>{
-      makeLeave(getImage(index))
-      .one('transitionend',(e)=>{
-        makeEnter($(e.currentTarget))
-      })  
-      makeCurrent(getImage(index+1))
-      index++
-    },1000)
   }
+  $slides.css({transform : 'translateX(-600px)'})
+  current = 0
 })
+$buttons.eq(1).on('click',function(e){
+  $slides.css({transform : 'translateX(-1200px)'})
+  current = 1
+})
+$buttons.eq(2).on('click',function(e){
+  $slides.css({transform : 'translateX(-1800px)'})
+  current = 2
+})
+$buttons.eq(3).on('click',function(e){
+  if(current===0){
 
-
-
-function getImage(index){
-  return $(`.images > img:nth-child(${x(index)})`)
-}
-
-function x(index){
-  if(index>4){
-    index%=4
-   if(index===0){
-    index=4
-   }
   }
-  return index
-}
-
-function makeLeave($node){
-  return $node.removeClass('current enter').addClass('leave')
-}
-
-function makeCurrent($node){
-  return $node.removeClass('leave enter').addClass('current')
-}
-
-function makeEnter($node){
-  return $node.removeClass('leave current').addClass('enter')
-}
-
-function init(){
-  index =1
-  $(`.images > img:nth-child(${index})`).addClass('current')
-  .siblings().addClass('enter')
-}
+  $slides.css({transform : 'translateX(-2400px)'})
+  current = 3
+})
